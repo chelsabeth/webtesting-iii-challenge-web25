6f64abba40db5633081af838eaf52b5415835ac5
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-12-16 17:01:32 -0500

    tests for Controls

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -7,3 +7,14 @@ import Dashboard from "./Dashboard";
 test("Dashboard renders without crashing", () => {
     render(<Dashboard/>);
 });
+
+// From Controls.js
+test("gates defaults are unlocked and open", () => {
+    const dashboard = render(<Dashboard/>);
+
+    const lockBtn = dashboard.getByText(/lock gate/i);
+    const openBtn = dashboard.getByText(/close gate/i);
+
+    expect(lockBtn.disabled).toBe(true);
+    expect(openBtn.disabled).toBe(false);
+});
