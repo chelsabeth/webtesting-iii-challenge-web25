65694aa6613dcc534bac20271343d9de5ac468f7
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-12-16 16:43:16 -0500

    render tests for each component

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,9 @@
-// Test away
+// Test away!
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Dashboard from "./Dashboard";
+
+test("Dashboard renders without crashing", () => {
+    render(<Dashboard/>);
+});
