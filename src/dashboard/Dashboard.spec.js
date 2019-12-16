b1ac01a4934ff03e60bd3bf808c5085b05159814
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-12-16 17:26:54 -0500

    tests done for toggleing and open gate

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,6 +1,6 @@
 // Test away!
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 import Dashboard from "./Dashboard";
 
@@ -8,13 +8,33 @@ test("Dashboard renders without crashing", () => {
     render(<Dashboard/>);
 });
 
-// From Controls.js
 test("gates defaults are unlocked and open", () => {
     const dashboard = render(<Dashboard/>);
 
-    const lockBtn = dashboard.getByText(/lock gate/i);
+    const lockGate = dashboard.getByText(/lock gate/i);
     const openBtn = dashboard.getByText(/close gate/i);
 
-    expect(lockBtn.disabled).toBe(true);
+    expect(lockGate.disabled).toBe(true);
     expect(openBtn.disabled).toBe(false);
 });
+
+test("gate opens", () => {
+    const { getByText, findByText } = render(<Dashboard/>);
+    fireEvent.click(getByText("Close Gate"));
+    findByText(/open gate/i);
+});
+
+test("gate cannot be closed or open if it is locked", () => {
+    const dashboard = render(<Dashboard/>);
+
+    // test all these conditions when the gate is locked
+    // gate can only be unlocked 
+    const closeBtn = dashboard.getByText(/close gate/i);
+    fireEvent.click(closeBtn);
+
+    const lockBtn = dashboard.getByText(/lock gate/i);
+    fireEvent.click(lockBtn);
+
+    const openBtn = dashboard.getByText(/open gate/i);
+    fireEvent.click(openBtn);
+})
